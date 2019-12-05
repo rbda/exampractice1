24403fac72471e71d6f7bfb9de3812d2680bdfe8
[rryanburton] <ryan.burton@da.org>
2019-12-05 12:43:03 -0500

    added first models

--- giftlistapp/models.py
+++ giftlistapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=250)
+
+
+class Clothes(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    size = models.CharField(max_length=30)
+    name = models.CharField(max_length=250)
+    # price
+    # url
+    # image
+
+
